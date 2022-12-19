750244f1980cbd118e707d43f52e22b20f1bc7aa
MChlebosz <mateusz.chlebosz@outlook.com>
2022-12-19 09:52:32 +0100

    c templates

--- server.c
+++ server.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef _WIN32
+	#include <windows.h>
+	#include <winsock2.h>
+#else
+	#include <arpa/inet.h>
+	#include <netinet/in.h>
+	#include <sys/socket.h>
+	#include <unistd.h>
+#endif    //
 
+int main(int argc, char *argv[]) {
+	return 0;
+}
