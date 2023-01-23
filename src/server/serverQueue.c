47ed3fa197ddefb0cb27320f989c344e155f506b
MChlebosz <mateusz.chlebosz@outlook.com>
2023-01-23 02:08:05 +0100

    Readme Change and Refactor

--- src/server/serverQueue.c
+++ src/server/serverQueue.c
@@ -207,7 +207,7 @@ void serve(int* keep_running, int* msgid, char* db) {
 
 				msgsnd(sessionQueue, &msg, sizeof(msg), 0);
 
-								continue;
+				continue;
 			} else if (status == 404) {
 				// internal server error - error opening database
 				//  send response message to sender
@@ -248,83 +248,6 @@ void serve(int* keep_running, int* msgid, char* db) {
 	free(sessions.sessions);
 }
 
-int openSession(int* sessionRunning, int* sessionQueue, char* clientID,
-				int clientSeed, key_t* sessionKey, int* sessionPID) {
-	// create the message queue for session, then in session connect to
-	// clientQueue create random session key based on client key
-	int sessionSeed = rand();
-	*sessionKey     = ftok(clientID, sessionSeed);
-	// create session queue
-	*sessionQueue   = msgget(*sessionKey, 0666 | IPC_CREAT);
-	*sessionRunning = 1;
-	// create session subprocess
-	*sessionPID = fork();
-	if (*sessionPID == 0) {
-		// send session key to client
-		//  connect to client queue
-		key_t clientKey = ftok(clientID, clientSeed);
-		int clientQueue = msgget(clientKey, 0666 | IPC_CREAT);
-		Message msg;
-		// send response message
-		// message format: clientID;sessionSeed;
-		char* MessageBody = strcat(clientID, ";");
-		char stringSessionSeed[10];
-		sprintf(stringSessionSeed, "%d", sessionSeed);
-		MessageBody = strcat(MessageBody, stringSessionSeed);
-		MessageBody = strcat(MessageBody, ";");
-
-		msgInit(&msg, 21, 1, "session", msg.mtext.header.sender, 200,
-				MessageBody);
-		// send response message
-		msgsnd(clientQueue, &msg, sizeof(msg), 0);
-
-		// serve session
-		session(sessionRunning, *sessionKey);
-
-	} else {
-		return 200;
-	}
-	*sessionRunning = 0;
-	return 500;
-}
-
-void addSession(Sessions* sessions, Session session) {
-	// increase sessions list size
-	sessions->size++;
-	// allocate memory for new session
-	sessions->sessions =
-		realloc(sessions->sessions, sessions->size * sizeof(Session));
-	// add new session to sessions list
-	sessions->sessions[sessions->size - 1] = session;
-}
-
-void removeSession(Sessions* sessions, char* sessionID) {
-	// find session in sessions list
-	for (int i = 0; i < sessions->size; i++) {
-		if (sessions->sessions[i].sessionID == sessionID) {
-			// remove session from sessions list
-			for (int j = i; j < sessions->size - 1; j++) {
-				sessions->sessions[j] = sessions->sessions[j + 1];
-			}
-			// decrease sessions list size
-			sessions->size--;
-			// reallocate memory for sessions list
-			sessions->sessions =
-				realloc(sessions->sessions, sessions->size * sizeof(Session));
-			break;
-		}
-	}
-}
-
-int getSessionQueue(Sessions* sessions, char* sessionID) {
-	// find session in sessions list
-	for (int i = 0; i < sessions->size; i++) {
-		if (sessions->sessions[i].sessionID == sessionID) {
-			return sessions->sessions[i].sessionQueue;
-		}
-	}
-	return 404;
-}
 int registerUser(char* username, char* password, char** key, char* db) {
 	// check if username is already taken
 	if (searchData(db, username) == 200) {
--- src/server/serverQueue.h
+++ src/server/serverQueue.h
@@ -11,36 +11,8 @@
 #include "../codes.h"
 #include "../message.h"
 
-// session type
-typedef struct {
-	char* sessionID;
-	int sessionRunning;
-	key_t sessionKey;
-	int sessionQueue;
-	int sessionPID;
-} Session;
-
-// sessions list
-typedef struct {
-	int size;
-	Session* sessions;
-} Sessions;
-
-// add session to sessions list
-void addSession(Sessions* sessions, Session session);
-
-// remove session from sessions list
-void removeSession(Sessions* sessions, char* sessionID);
-
-int getSessionQueue(Sessions* sessions, char* sessionID);
-
 void serve(int* keep_running, int* msgid, char* db);
 
 int registerUser(char* username, char* password, char** key, char* db);
-
-int openSession(int* sessionRunning, int* sessionQueue, char* clientID,
-				int clientSeed, key_t* sessionKey, int* sessionPID);
-
-char* generateKey();
 
 int loginUser(char* username, char* password, char** key, char* db);
